daa9ae7e7ac7ea51c511ac3c5f785ea2a1bcecd6
Yosefariel <yosefariel770@gmail.com>
2021-08-04 18:44:25 +0300

    task_3

--- nodejsProject/task_3.js
+++ nodejsProject/task_3.js
@@ -1,13 +1,26 @@
 var fs = require('fs')
 
 
-file_mover=()=>{
+var fs = require('fs')
+var path = require('path');
+const join = require('path');
 
-    const oldPath = './files_to_move/path/file.txt'
-    const newPath = 'moved_files/path/file.txt'
-    
-    fs.rename(oldPath, newPath, function (err) {
-      if (err) throw err
-      console.log('Successfully renamed - AKA moved!')
-    })
+
+var oldPath = path.join(__dirname,'files_to_move');
+var newPath = path.join(__dirname,'moved_files');
+const file_list = fs.readdirSync(oldPath);
+
+function file_mover() {
+    for(i=0; i < file_list.length; i++ ) {
+       fs.rename(`${oldPath}/${file_list[i]}`, `${newPath}/${file_list[i]}`, function (err) {
+    if (err) throw err
+})
+       fs.appendFile('moved_files.txt', `${file_list[i]}\n`, function (err) {
+    if (err) throw err;
+})
+    console.log(`${file_list[i]}`);
+ }
 }
+  
+  file_mover();
+  
--- nodejsProject/task_jokes.js
+++ nodejsProject/task_jokes.js
@@ -4,25 +4,24 @@ const { Console } = require('console');
 require('dotenv').config();
 
 
-const AmountJoke=process.env.JOKE_AMOUNT || 50;
-const SubjectJoke=process.env.JOKE_SUBJECT;
-console.log(AmountJoke);
-
+const amountJoke=process.env.JOKE_AMOUNT || 50;
+const subjectJoke=process.env.JOKE_SUBJECT;
 
 function jokes() {
-    for(let i = 0; i < AmountJoke; i++){
-        let getWithTag = jokes1.getRandomJokeWithTag(SubjectJoke);
+    for(let i = 0; i < amountJoke; i++){
+        let getWithTag = jokes1.getRandomJokeWithTag(subjectJoke);
         let conversion=JSON.stringify(getWithTag);
    fs.readFile('jokes.txt', 'utf8',function (err,date) {
-      if (date.includes(conversion)) throw err; 
+      if(date.includes(conversion)) throw err; 
     fs.appendFile('jokes.txt', ` This is my text: ${conversion}\n`,function(err){
-        if(err) throw err;
-    })
+      if(err) throw err;
       })
-    }
-  }
+    })
+   }
+ }
 
-  if(AmountJoke < 50){
+  
+  if(amountJoke < 50){
     console.log('block not fun')
 } else {
     jokes();  
