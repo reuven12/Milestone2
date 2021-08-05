56805d7d8ccb85b5810023837e27fdeb918dc8d4
reuven12 <86264874+reuven12@users.noreply.github.com>
2021-08-05 12:15:43 +0300

    Update task_3.js
    finished task_3

--- nodejsProject/task_3.js
+++ nodejsProject/task_3.js
@@ -3,21 +3,24 @@ const path = require('path');
 const join = require('path');
 
 
-const oldPath = path.join(__dirname,'files_to_move');
-const newPath = path.join(__dirname,'moved_files');
-const file_list = fs.readdirSync(oldPath);
 
 function file_mover() {
+  const oldPath = path.join(__dirname,'files_to_move');
+  const newPath = path.join(__dirname,'moved_files');
+  const file_list = fs.readdirSync(oldPath);
     for(i=0; i < file_list.length; i++ ) {
-       fs.rename(`${oldPath}/${file_list[i]}`, `${newPath}/${file_list[i]}`, function (err) {
+      fs.rename(`${oldPath}/${file_list[i]}`, `${newPath}/${file_list[i]}`, function (err) {
     if (err) throw err
 })
        fs.appendFile('moved_files.txt', `${file_list[i]}\n`, function (err) {
     if (err) throw err;
+
 })
-    console.log(`${file_list[i]}`);
+console.log(`${file_list[i]}`);
+file_list.length = 0;
  }
 }
-  
-  file_mover();
+
+setInterval(file_mover, 1500);
+
   
